22cde0644aba392f80b9dcac7a0206b917d85637
MrAziz007 <isido7094@gmail.com>
2024-05-12 15:00:00 +0500

    формула с reduce

---
+++ MrAziz.js
@@ -0,0 +1,42 @@
+// let arr = [
+// {
+//   id: 1,
+//   name: "Leanne Graham",
+//   username: "Bret",
+//   email: "Sincere@april.net",
+// },
+// {
+//   id: 2,
+//   name: "Ervin Howell",
+//   username: "Antonette",
+//   email: "Shanna@melissa.org",
+// },
+// {
+//   id: 3,
+//   name: "Clementine Bauch",
+//   username: "Samantha",
+//   email: "Nathan@yesenia.net",
+// },
+// {
+//   id: 4,
+//   name: "Patricia Lebsack",
+//   username: "Karianne",
+//   email: "Julianne.OConner@kory.org",
+// },
+// ];
+// let net = [];
+// let org = [];
+// arr.filter(item => item.email.slice(-3) == 'org' ? org.push(item) : net.push(item))
+// console.log(net, org);
+
+// let students = [
+//   { name: 'Alice', age: 22 },
+//   { name: 'Bob', age: 20 },
+//   { name: 'Charlie', age: 24 },
+//   { name: 'David', age: 21 }
+// ];
+// let totalAge = students.reduce((one, two) => one + two.age / students.length, 0);
+// console.log(totalAge);
+// let temp = [];
+// students.filter(item => item.age >= 22 ? temp.push(item) : 0);
+// console.log(temp);
